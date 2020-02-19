ef45a8edfe827517a69ee3c6a34c3cad8bdd77cb
Vincent <vincentlie123@gmail.com>
2020-02-19 19:04:40 +0700

    commit 2

--- UTS.go
+++ UTS.go
@@ -11,15 +11,16 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	count := 0
 	for scanner.Scan() {
-		count++
 		kata2 := strings.Split(scanner.Text()," ")
 		m := make(map[string]int) 
 		for _, kata := range kata2 { 
-		   m[kata] += 1 
+			count++
+		   	m[kata] += 1 
 	   }
 	   fmt.Println(kata2)
 	   fmt.Println()
 	   fmt.Println(m)
+	   fmt.Println("word count = " , count)
 	}
 
 }
